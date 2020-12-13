cf0f8f3096b551b389d60721de4b67dde9985c3b
jparba <geminix2793@gmail.com>
2020-12-14 01:38:26 +0400

    test api push

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/data', function (Request $request) {
+    return [
+    	'phished_data' => [
+    		[
+	    		'username' 	=> 'haha',
+	    		'password'	=> 'hadadjlad'
+    		],
+
+    		[
+	    		'username' 	=> 'haha',
+	    		'password'	=> 'hadadjlad'
+    		],
+
+    		[
+	    		'username' 	=> 'haha',
+	    		'password'	=> 'hadadjlad'
+    		],
+
+    	]
+    ];
 });
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
